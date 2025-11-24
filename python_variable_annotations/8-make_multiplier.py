fefa1da1d2a327364b18ed9659fb6914cc59c954
loicleguen <loic.leguen@hotmail.fr>
2025-11-24 11:57:49 +0100

    Refactor make_multiplier function to improve readability and add compiled bytecode

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""Module that defines a function to multiply two numbers with type annotations."""
+"""Module that defines a function to multiply
+two numbers with type annotations."""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float, number: float) -> float:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns the product of multiplier and number."""
-    return multiplier * number
+    def multiply(n: float) -> float:
+        return n * multiplier
+    return multiply
