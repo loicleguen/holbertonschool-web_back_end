188baf1a5476889381c89cf3983e618d5e160c47
loicleguen <loic.leguen@hotmail.fr>
2025-11-24 11:07:33 +0100

    Add type annotations to the add function and define new variables with type annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-''' adding type annotations to the add function'''
+"""adding type annotations to the add function"""
 
 
 def add(a: float, b: float) -> float:
-    ''' adds two float numbers and returns a float '''
+    """adds two float numbers and returns a float"""
     return a + b
