faddbd71fd1eead93ee1432a0ae41a950cb100f1
loicleguen <loic.leguen@hotmail.fr>
2025-11-24 11:48:52 +0100

    Add make_multiplier function with type annotations to multiply two float numbers

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines a function to multiply two numbers with type annotations."""
+
+
+def make_multiplier(multiplier: float, number: float) -> float:
+    """Returns the product of multiplier and number."""
+    return multiplier * number
