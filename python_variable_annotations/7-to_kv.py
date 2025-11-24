d13ef40d413958623fb24f9025f3f2eca5cfad28
loicleguen <loic.leguen@hotmail.fr>
2025-11-24 11:45:33 +0100

    Fix type annotation in to_kv function to use Tuple instead of tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Module that contains a function with type annotations"""
-from typing import List, Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple containing a string and an integer"""
     return (k, float(v * v))
