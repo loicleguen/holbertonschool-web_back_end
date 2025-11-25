d0815a022a0d2f724748c756fa47678091db17b5
loicleguen <loic.leguen@hotmail.fr>
2025-11-25 15:30:01 +0100

    Add async_comprehension.py to collect 10 random numbers using async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a coroutine that collects 10 random numbers
+using an async comprehension over async_generator."""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random numbers using async comprehension."""
+    return [number async for number in async_generator()]
