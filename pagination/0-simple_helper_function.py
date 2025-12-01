dab77e3eb29cfadc1bed458562c9d7baae26f6c1
loicleguen <loic.leguen@hotmail.fr>
2025-12-01 09:28:11 +0100

    Refactor code structure for improved readability and maintainability

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Simple helper function for pagination """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ Return a tuple of start and end index for pagination."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
