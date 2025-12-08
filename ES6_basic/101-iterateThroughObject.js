044dc76958cb81c8609fe6a25ab2aa97f1fa8a1a
loicleguen <loic.leguen@hotmail.fr>
2025-12-08 13:09:43 +0100

    Add iterateThroughObject function to join employee names into a string

---
+++ ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const name of reportWithIterator) {
+    employees.push(name);
+  }
+  return employees.join('|');
+}
