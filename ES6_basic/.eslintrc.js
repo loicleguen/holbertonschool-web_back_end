80e62bbfda15c47c4d0cff3c640ce42141f874fb
loicleguen <loic.leguen@hotmail.fr>
2025-12-08 09:55:21 +0100

    Fix indentation in 0-constants.js for consistency

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "env": {
         "browser": true,
+        node: true,
         "es6": true
     },
     "extends": "eslint:recommended",
@@ -13,5 +14,12 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        semi: ['error', 'always'],           // Require semicolons
+        'prefer-const': 'error',             // Prefer const
+        indent: ['error', 2],                // 2 spaces indentation
+        quotes: ['error', 'single'],         // Single quotes
+        'no-unused-vars': 'warn',            // Warn for unused variables
+        'no-console': 'warn',                // Warn for console.log
+        'eol-last': ['error', 'always'],     // Newline at end of file
     }
 };
