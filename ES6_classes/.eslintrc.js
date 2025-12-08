89d457d9b77112188eec9bd11f608d6993aa1d46
loicleguen <loic.leguen@hotmail.fr>
2025-12-08 13:40:26 +0100

    Add package.json with scripts and devDependencies for ES6 classes project

---
+++ ES6_classes/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        node: true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        semi: ['error', 'always'],           // Require semicolons
+        'prefer-const': 'error',             // Prefer const
+        indent: ['error', 2],                // 2 spaces indentation
+        quotes: ['error', 'single'],         // Single quotes
+        'no-unused-vars': 'warn',            // Warn for unused variables
+        'no-console': 'warn',                // Warn for console.log
+        'eol-last': ['error', 'always'],     // Newline at end of file
+        'arrow-spacing': ['error', { 'before': true, 'after': true }], // Enforce space around =>
+    }
+};
