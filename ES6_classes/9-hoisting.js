ab0cc672a2246bb38d940943ae28ed31ebb723e1
loicleguen <loic.leguen@hotmail.fr>
2025-12-08 16:04:33 +0100

    Refactor HolbertonClass instantiation to avoid duplication in code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -13,6 +13,9 @@ export class HolbertonClass {
   }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -33,8 +36,6 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
