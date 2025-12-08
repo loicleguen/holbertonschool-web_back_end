bc82322316f7448d222bc54e560520db63b1a700
loicleguen <loic.leguen@hotmail.fr>
2025-12-08 09:47:34 +0100

    Remove redundant comments and improve clarity in 0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-// Use const for variables that won't be reassigned
 export function taskFirst() {
 const task = 'I prefer const when I can.';
   return task;
 }
 
-// Simple function returning a string
 export function getLast() {
   return ' is okay';
 }
 
-// Use let for variables that may be reassigned
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
