9c47ac06d4b20ab2d9b7a6d6b96b04542d7bed37
loicleguen <loic.leguen@hotmail.fr>
2025-12-15 11:06:08 +0100

    Fix cleanSet function to handle undefined startString case

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (startString.length === 0 || startString === undefined) {
     return '';
   }
   return Array.from(set)
