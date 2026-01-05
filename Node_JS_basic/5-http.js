1fa7e29523bb447f39b2ee290b83b2987e91c0e3
loicleguen <loic.leguen@hotmail.fr>
2026-01-05 15:09:03 +0100

    Refactor HTTP server to correctly handle root and students routes

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,8 +4,10 @@ const countStudents = require('./3-read_file_async');
 const databasePath = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  if (req.url === '/students') {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }else if (req.url === '/students') {
     let output = 'This is the list of our students\n';
 
     const originalLog = console.log;
@@ -24,8 +26,6 @@ const app = http.createServer(async (req, res) => {
       output += error.msg;
       res.end(output);
     }
-  } else if (req.url === '/') {
-    res.end('Hello Holberton School!');
   } else {
     res.statusCode = 404;
     res.end('not found');
