20abdfeba9df1a6f9aa8879af27ed31ac0a1ae0f
loicleguen <loic.leguen@hotmail.fr>
2026-01-05 14:52:28 +0100

    Add HTTP server to handle student data requests and respond with a greeting

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const databasePath = process.argv[2];
+
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/students') {
+    let output = 'This is the list of our students\n';
+
+    const originalLog = console.log;
+    const logs = [];
+    console.log = (msg) => {
+      logs.push(msg);
+    };
+
+    try {
+      await countStudents(databasePath);
+      console.log = originalLog;
+      output += logs.join('\n');
+      res.end(output);
+    } catch (error) {
+      console.log = originalLog;
+      output += error.msg;
+      res.end(output);
+    }
+  } else {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+module.exports = app;
