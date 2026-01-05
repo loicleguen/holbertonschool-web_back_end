c1d424baa55d9fcce66b5f267868ca018e28d3d7
loicleguen <loic.leguen@hotmail.fr>
2026-01-05 15:17:46 +0100

    Fix response header placement in HTTP server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,8 +4,8 @@ const countStudents = require('./3-read_file_async');
 const databasePath = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     let output = 'This is the list of our students\n';
